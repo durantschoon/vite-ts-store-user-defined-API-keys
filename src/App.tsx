6ca39d84e03add523daac0e587749f27377dbca0
durantschoon <durantschoon@users.noreply.github.com>
2023-01-03 11:39:41 -0500

    2 files modified, adding APIKey data

--- src/App.tsx
+++ src/App.tsx
@@ -6,10 +6,24 @@ import './App.css'
 const APIKeysSchema = z.object({
   shortName: z.string().optional(),
   url: z.string().url(),
-  apiKey: z.string()
+  apiKey: z.string().optional()
 })
 
 type APIKeys = Array<z.infer<typeof APIKeysSchema>>
+
+/* 
+  create a .env file in the repo (ignored in _gitignore)
+  Add lines for your keys, e.g.
+  UNSPLASH_API_KEY=YOUR_KEY_HERE
+*/ 
+
+keyData = [
+  {
+    shortName: 'unsplash',
+    url: 'https://api.unsplash.com/search/photos',
+    apiKey: import.meta.env.UNSPLASH_API_KEY
+  }
+]
 
 function App() {
   const [apiKeys, setApiKeys] = useState({} as APIKeys)
