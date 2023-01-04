5ecc27c3d8fc2636a5b3bbea45cba7ac7fba694b
Durant Schoon <durant.schoon@gmail.com>
2023-01-03 19:26:17 -0500

    renders a list of api key data with defaults

--- src/App.tsx
+++ src/App.tsx
@@ -1,45 +1,17 @@
 import { useState, useRef, MouseEvent } from 'react'
-import { z } from "zod";
+import ApiKeys, { defaultAPIKeyData } from "./components/ApiKey"
 
 import './App.css'
 
-const APIKeySchema = z.object({
-  shortName: z.string().optional(),
-  url: z.string().url(),
-  apiKey: z.string().optional()
-})
-
-type APIKeys = Array<z.infer<typeof APIKeySchema>>
-
-/* 
-  If you want to predefine your own keys, but keep them out of your git repo,
-  Create a .env file in this repo (which is has been ignored in _gitignore)
-  Add variables for your keys, e.g.
-  VITE_UNSPLASH_API_KEY=YOUR_KEY_HERE
-  Note that only variables beginning with 'VITE_' can be accessed by a vite app
-*/ 
-
-const apiKeyData: APIKeys = [
-  {
-    shortName: 'unsplash',
-    url: 'https://api.unsplash.com/search/photos',
-    apiKey: import.meta.env.VITE_UNSPLASH_API_KEY
-  }
-]
-
-console.log(import.meta.env)
-
 function App() {
-  const [apiKeys, setApiKeys] = useState(apiKeyData)
+  const [apiKeys, setApiKeys] = useState(defaultAPIKeyData)
   // const containerRef = useRef(null)
   // const cardRef = useRef(null)
 
   const handleClick = (event: MouseEvent) => {
     event.stopPropagation()    
     const apiKeysDiv = document.getElementById("apiKeysDiv")!
-    apiKeysDiv.classList.remove("apiKeysDiv-hidden")    
-    console.log("button was clicked!")
-    console.log(apiKeysDiv.classList)
+    apiKeysDiv.classList.remove("apiKeysDiv-hidden")   
   }
 
   return (
@@ -57,7 +29,7 @@ function App() {
           </div>
       </div>
       <div className="card apiKeysDiv-hidden" id="apiKeysDiv">
-        Hi!
+        <ApiKeys { ...{apiKeys} }/>
       </div>
     </div>
   )
